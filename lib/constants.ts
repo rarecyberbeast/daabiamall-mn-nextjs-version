20e8c9dea8f59ce30aa4862f486a8b076d586dfb
rarecyberbeast <cyberbeaststudio@gmail.com>
2025-04-14 17:19:51 +0000

    commiting to push remote changes

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'DAABIA MALL'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'easly trade with Pi'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A Web3.0 e-commerce platform for buying and selling, fueled with Pi.'
